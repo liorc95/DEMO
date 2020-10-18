c4e4f09dfae8587ddb50b42dd041fa08a158edf0
Lior Cohen <lior.cohen@whitesourcesoftware.com>
2020-10-18 18:17:51 +0300

    fix

--- src/main/java/xss/XSS1.java
+++ src/main/java/xss/XSS1.java
@@ -24,7 +24,7 @@ public class XSS1 {
             response.setContentType("text/html");
             PrintWriter out = response.getWriter();
             out.write("<br><br>Unsafe CSS style attribute context:<br>");
-            out.write("<div <style>color:" + name + "<style>>");
+            out.write("<div> <style>color:" + name + "</style>");
             out.write("<h1>TEXT</h1>");
             out.write("</div>");
         } catch (IOException e) {
