28f7033fccad2f8716b8a3dfbd82e6170525a69f
Lior Cohen <lior.cohen@whitesourcesoftware.com>
2020-10-19 18:22:07 +0300

    move uuid

--- src/main/java/sqlInjection/SQLI.java
+++ src/main/java/sqlInjection/SQLI.java
@@ -8,8 +8,8 @@ import java.util.UUID;
 public class SQLI {
     public void sqlTest1(String ip) {
         try {
+			String uuid = UUID.randomUUID().toString();
             Statement statement = getJDBCConnection().createStatement();
-            String uuid = UUID.randomUUID().toString();
             String sql = "INSERT INTO banned_ip(id, ip) VALUE('" + uuid + "','" + ip + "')";
             statement.execute(sql);
         } catch (SQLException exception) {
