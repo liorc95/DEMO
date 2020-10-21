4a40af22b14efd8b29dc34f6948ee3f685097f75
Lior Cohen <lior.cohen@whitesourcesoftware.com>
2020-10-21 12:55:18 +0300

    spoless

--- src/main/java/pathTraversal/PTTest1.java
+++ src/main/java/pathTraversal/PTTest1.java
@@ -4,20 +4,22 @@ import java.io.File;
 
 // Base Directory: /users/ws/profiles/
 // Unsafe filename: ../../../etc/passwd
-// Canonical unsafe-file: /users/ws/profiles/../../../etc/passwd ----->>>  /etc/passwd ---->>>> Vulnerability!
+// Canonical unsafe-file: /users/ws/profiles/../../../etc/passwd ----->>>  /etc/passwd ---->>>>
+// Vulnerability!
 
 public class PTTest1 {
-    private final String BASE_DIRECTORY = "/users/ws/profiles/";
-    public void test(String filename) {
-        try {
-            File myFile = new File(BASE_DIRECTORY + filename);
-            deleteFile(myFile);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+  private final String BASE_DIRECTORY = "/users/ws/profiles/";
 
-    private void deleteFile(File f) {
-        f.delete();
+  public void test(String filename) {
+    try {
+      File myFile = new File(BASE_DIRECTORY + filename);
+      deleteFile(myFile);
+    } catch (Exception ex) {
+      ex.printStackTrace();
     }
+  }
+
+  private void deleteFile(File f) {
+    f.delete();
+  }
 }
--- src/main/java/pathTraversal/PTTest3.java
+++ src/main/java/pathTraversal/PTTest3.java
@@ -1,22 +1,21 @@
 package pathTraversal;
 
-import java.util.UUID;
 import java.io.File;
 
 public class PTTest3 {
-    private final String BASE_DIRECTORY = "/users/ws/profiles/";
-    public void test(String fileName, String customDirName) {
-        try {
-            String dirPath = getValue(BASE_DIRECTORY) + customDirName + "subsubdir/";
-            File myFile = new File(dirPath + fileName);
-            myFile.delete();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+  private final String BASE_DIRECTORY = "/users/ws/profiles/";
 
-    public String getValue(String str){
-        return str+ "userdir/";
+  public void test(String fileName, String customDirName) {
+    try {
+      String dirPath = getValue(BASE_DIRECTORY) + customDirName + "subsubdir/";
+      File myFile = new File(dirPath + fileName);
+      myFile.delete();
+    } catch (Exception ex) {
+      ex.printStackTrace();
     }
+  }
 
+  public String getValue(String str) {
+    return str + "userdir/";
+  }
 }
