94819428afac8e5355e6a39a9df092686f068def
liorc95 <58390166+liorc95@users.noreply.github.com>
2020-11-03 11:02:13 +0200

    Update XSS1.java

--- src/main/java/xss/XSS1.java
+++ src/main/java/xss/XSS1.java
@@ -28,9 +28,8 @@ public class XSS1 {
       response.setContentType("text/html");
       PrintWriter out = response.getWriter();
       out.write("<br><br>Unsafe CSS style attribute context:<br>");
-      out.write("<div>" + name + "");
+      out.write("<div>" + name + "</div>");
       out.write("<h1>TEXT</h1>");
-      out.write("</div>");
     } catch (IOException e) {
       e.printStackTrace();
     }
