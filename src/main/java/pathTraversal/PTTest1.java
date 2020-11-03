763ad939c3d597c3ae47e7e472ab0a136d399eb4
liorc95 <58390166+liorc95@users.noreply.github.com>
2020-11-03 13:42:16 +0200

    Update PTTest1.java

--- src/main/java/pathTraversal/PTTest1.java
+++ src/main/java/pathTraversal/PTTest1.java
@@ -12,33 +12,13 @@ public class PTTest1 {
 
   public void test(String filename) {
     try {
-      String newBase = getbaseDir(BASE_DIRECTORY);
-      File myFile = new File(newBase + filename);
+      File myFile = new File(BASE_DIRECTORY);
       deleteFile(myFile);
     } catch (Exception ex) {
       ex.printStackTrace();
     }
   }
-  
-  private String getbaseDir(String base){
-    return base +"subDir";
-  }
 
-  private void getFile1(String fileName){
-    getFile2(fileName);
-  }
-  
-private void getFile2(String fileName){
-  getFile3(fileName);
-  }
-  private void getFile3(String fileName){
-    getFile4(fileName);
-  }
-  private void getFile4(String fileName){
-    test(fileName);
-  }
- 
-  
   private void deleteFile(File f) {
     f.delete();
   }
