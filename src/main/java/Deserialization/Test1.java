0bd4be67358f93e60d0bfc9e237da610603a84f5
Nadav <barakna@post.bgu.ac.il>
2020-11-17 12:21:16 +0200

    Spotless

--- src/main/java/Deserialization/Test1.java
+++ src/main/java/Deserialization/Test1.java
@@ -1,42 +1,42 @@
 package Deserialization;
 
-import java.io.IOException;
 import java.io.FileInputStream;
-import java.util.Set;
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Set;
 
 public class Test1 {
-    public void test(FileInputStream fileIS) throws IOException, ClassNotFoundException {
+  public void test(FileInputStream fileIS) throws IOException, ClassNotFoundException {
 
-        ObjectInputStream in = new ObjectInputStream(fileIS);
-        ParentClass userParent = (ParentClass) in.readObject();
-        ChildClass userChild = (ChildClass) userParent;
-        in.close();
-    }
+    ObjectInputStream in = new ObjectInputStream(fileIS);
+    ParentClass userParent = (ParentClass) in.readObject();
+    ChildClass userChild = (ChildClass) userParent;
+    in.close();
+  }
 }
 
 class ParentClass {
-    private int val;
+  private int val;
 
-    public ParentClass(int value){
-        this.val = value;
-    }
+  public ParentClass(int value) {
+    this.val = value;
+  }
 }
 
 class ChildClass extends ParentClass {
-    private ObjectClass obj = null;
+  private ObjectClass obj = null;
 
-    public ChildClass(int value){
-        super(value);
-        this.obj = new ObjectClass(value);
-    }
+  public ChildClass(int value) {
+    super(value);
+    this.obj = new ObjectClass(value);
+  }
 }
 
 class ObjectClass {
-    private int val;
-    private Set<String> setVal = null;
+  private int val;
+  private Set<String> setVal = null;
 
-    public ObjectClass(int value){
-        this.val = value;
-    }
+  public ObjectClass(int value) {
+    this.val = value;
+  }
 }
